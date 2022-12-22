629f3b2cd366d49b50a4788d25a64f05783b12f6
Vladislav Borodin <vladboro@mail.ru>
2022-12-22 14:13:50 +0300

    Final

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import ReactDOM from "react-dom/client";
 
-const App = () => <h1>Hello, World!</h1>;
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const getCBR = (minute) => {
+    console.log("getCBR start");
+    for (let i = 0; i < 100000; i++) {
+      for (let j = 0; j < 100000; j++) {
+        const element = 100000;
+      }
+    }
+    console.log("getCBR end");
+    return `Rate for ${minute}: 67.89`;
+  };
+  let minute = new Date().getMinutes().toString();
+  let rate = useMemo(() => getCBR(minute), [minute]);
+
+  return (
+    <div>
+      <button type="button" onClick={() => setCount(count + 1)}>
+        {count}
+      </button>
+      <h1>{rate}</h1>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
